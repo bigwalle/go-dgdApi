402f88783d91b4202724bc8079b4f58359942072
wuxiangyang <292545811@qq.com>
2018-10-31 14:58:31 +0800

    werw

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello World11!2q1rewwewew12ww2233")
+		c.String(http.StatusOK, "Hello World11!2q1rewwewew12ww223wer")
 	})
 	router.Run(":8088")
 
