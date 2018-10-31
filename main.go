262d2172c0b17a3e36acf6b19bfc7fc2b789bd93
wuxiangyang <292545811@qq.com>
2018-10-31 14:57:00 +0800

    Signed-off-by: wuxiangyang <292545811@qq.com>

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello World11!2q1rewwewew12ww22")
+		c.String(http.StatusOK, "Hello World11!2q1rewwewew12ww2233")
 	})
 	router.Run(":8088")
 
