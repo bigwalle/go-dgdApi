c4d0cae4cf2d501cc91f59360f7d6d0fe652a92d
wuxiangyang <292545811@qq.com>
2018-10-31 11:43:19 +0800

    测试！

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello World!")
+		c.String(http.StatusOK, "Hello World1!")
 	})
 	router.Run(":8000")
 
